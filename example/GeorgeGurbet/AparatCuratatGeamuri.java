0b968ff720e3b8653f1f1920d4e7c8ef1b5de96d
George Gurbet <georgegurbet4@gmail.com>
2025-12-11 08:09:23 +0200

    Update AparatCuratatGeamuri.java

--- example/GeorgeGurbet/AparatCuratatGeamuri.java
+++ example/GeorgeGurbet/AparatCuratatGeamuri.java
@@ -1,7 +1,4 @@
 package org.example.GeorgeGurbet;
 import org.example.Aparat;
 public class AparatCuratatGeamuri {
-    pardoseli[0] = ("Marca", "Model", 0)
-    pardoseli[1] = ("Cleanex", "Kober" , 183.62)
-    pardoseli[2] = ("FL300", "Deichman", 264.49)
 }
