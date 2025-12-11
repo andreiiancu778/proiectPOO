34f37f721906d602c34754d7a339db79c040b373
George Gurbet <georgegurbet4@gmail.com>
2025-12-11 08:00:25 +0200

    Add classes

--- Gurbet George - Aparate curatat/AparatCuratat.java
+++ Gurbet George - Aparate curatat/AparatCuratat.java
@@ -1,1 +1,47 @@
+class AparatCuratatPardoseli extends Aparat {
+    private int putere;
 
+    public AparatCuratatPardoseli() {
+        super();
+        this.putere = 0;
+    }
+
+    public AparatCuratatPardoseli(String marca, String model, int putere) {
+        super(marca, model);
+        this.putere = putere;
+    }
+
+    public AparatCuratatPardoseli(String marca, String model, double pret, int putere) {
+        super(marca, model, pret);
+        this.putere = putere;
+    }
+
+    @Override
+    public String toString() {
+        return "AparatCuratatPardoseli [" + super.toString() + ", putere=" + putere + "]";
+    }
+}
+
+class AparatCuratatGeamuri extends Aparat {
+    private double capacitateRezervor;
+
+    public AparatCuratatGeamuri() {
+        super();
+        this.capacitateRezervor = 0.0;
+    }
+
+    public AparatCuratatGeamuri(String marca, String model, double capacitateRezervor) {
+        super(marca, model);
+        this.capacitateRezervor = capacitateRezervor;
+    }
+
+    public AparatCuratatGeamuri(String marca, String model, double pret, double capacitateRezervor) {
+        super(marca, model, pret);
+        this.capacitateRezervor = capacitateRezervor;
+    }
+
+    @Override
+    public String toString() {
+        return "AparatCuratatGeamuri [" + super.toString() + ", capacitateRezervor=" + capacitateRezervor + "]";
+    }
+}
